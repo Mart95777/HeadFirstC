341d306c8d5c2f8eedca34878113620ad3ffa0a6
Mart95777 <marcin.m.niewiadomski@gmail.com>
2018-08-15 00:06:55 +0200

    gitignore & page 176

---
+++ chapter04/p176.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+float mercury_day_in_earth_days();
+int hours_in_earth_day();
+
+int main()
+{
+  //
+  
+  printf("A day on Mercury is %f hours\n", day);
+  return 0;
+}
+
+float mercury_day_in_earth_days()
+{
+  return 58.65;
+}
+
+int hours_in_earth_day()
+{
+  return 24;
+}
